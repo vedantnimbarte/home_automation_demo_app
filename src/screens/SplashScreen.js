234fb22ef990a668ca65876ad58131b91b23be50
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-16 17:34:59 +0530

    Added SplashScreen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import {View, Image} from 'react-native';
+import {SIZES} from '../constants';
 
 export default class SplashScreen extends React.Component {
   render() {
     return (
-      <View>
-        <Image source={require('../assets/logo.png')} style={styles.logo} />
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image source={require('../assets/logo.png')} style={styles.logo} />
+        </View>
       </View>
     );
   }
 }
 
 const styles = {
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    padding: 10,
+    top: SIZES.Height * 0.3,
+  },
   logo: {
-    height: 20,
-    width: 20,
+    height: SIZES.Height * 0.21,
+    width: SIZES.Width * 0.4,
   },
 };
