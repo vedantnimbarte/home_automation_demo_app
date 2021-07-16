647a1714f570062b4153af03c86d7a249af08f3f
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-16 17:43:17 +0530

    Added login page to navigator

--- src/navigation/Auth.navigation.js
+++ src/navigation/Auth.navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {SplashScreen} from '../screens';
+import {SplashScreen, Login} from '../screens';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +8,7 @@ export const AuthNavigation = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="SplashScreen" component={SplashScreen} />
+      <Stack.Screen name="LoginScreen" component={Login} />
     </Stack.Navigator>
   );
 };
