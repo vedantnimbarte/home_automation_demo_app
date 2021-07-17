d0afd9dab906b48581062e4e4a274e396c7417bc
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-17 18:23:27 +0530

    Added some header options to home navigator

--- src/navigation/Home.navigation.js
+++ src/navigation/Home.navigation.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Home} from '../screens';
+import {COLORS, FONTS} from '../constants/theme';
 
 const Stack = createStackNavigator();
 
 export const HomeNavigation = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="HomeScreen" component={Home} />
+    <Stack.Navigator
+      detachInactiveScreens={true}
+      screenOptions={{
+        headerTitle: 'Home Automation',
+        headerTitleAlign: 'center',
+        headerTintColor: COLORS.White,
+        headerStyle: {
+          backgroundColor: COLORS.Primary,
+        },
+        headerTitleStyle: {
+          fontFamily: FONTS.Primary,
+        },
+      }}>
+      <Stack.Screen
+        name="HomeScreen"
+        component={Home}
+        options={{
+          headerLeft: false,
+        }}
+      />
     </Stack.Navigator>
   );
 };
