941c7fdf1f07258eec74020fca7104a7d28e09fa
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-17 16:47:41 +0530

    fixed home screen and splash screen import string

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,5 @@
-import SplashScreen from './SplashScreen';
+import {SplashScreen} from './SplashScreen';
 import {Login} from './Login';
-import Home from './Home';
+import {Home} from './Home';
 
 export {SplashScreen, Login, Home};
