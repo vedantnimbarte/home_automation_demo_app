4cc1929a610329783b53eaba8522f6268cb04331
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-17 12:42:42 +0530

    Removed react native paper

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-import {Provider as PaperProvider} from 'react-native-paper';
 
-export default function Main() {
-  return (
-    <PaperProvider>
-      <App />
-    </PaperProvider>
-  );
-}
-
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => App);
