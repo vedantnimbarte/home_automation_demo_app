c1b13801aa4d41beacd200840162ffb27bbce9a3
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-17 18:28:27 +0530

    Added new import of add product page

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,6 @@
 import {SplashScreen} from './SplashScreen';
 import {Login} from './Login';
 import {Home} from './Home';
+import {Add_Product} from './AddProduct';
 
-export {SplashScreen, Login, Home};
+export {SplashScreen, Login, Home, Add_Product};
