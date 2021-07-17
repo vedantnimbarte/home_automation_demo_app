ee0cfc39150da4de06df0961e3db8c2a548aecc9
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-17 18:27:16 +0530

    Created screen to add new product

---
+++ src/screens/AddProduct.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+export const Add_Product = () => {
+  return (
+    <View>
+      <Text>Add Product</Text>
+    </View>
+  );
+};
