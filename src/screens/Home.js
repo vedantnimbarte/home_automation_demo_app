bd0b047a09fa70f0511d367623b789edcb2df5be
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-17 15:31:24 +0530

    Created home page

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {SIZES, COLORS, FONTS} from '../constants/theme';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Home Page</Text>
+      </View>
+    );
+  }
+}
