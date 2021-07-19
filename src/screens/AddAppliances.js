902d2cd188c4e421cf5c83b3680439f649ce4348
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-19 15:57:58 +0530

    Created add appliances screen

---
+++ src/screens/AddAppliances.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const AddAppliances = () => {
+  return (
+    <View>
+      <Text>Add Appliances</Text>
+    </View>
+  );
+};
