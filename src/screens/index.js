cf1039a60d57bcda8a359e5db1bac7578ad39525
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-19 14:20:30 +0530

    Added configure home page path to screens index

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,5 +3,6 @@ import {Login} from './Login';
 import {Home} from './Home';
 import {Add_Product} from './AddProduct';
 import {Product} from './Product';
+import {ConfigureHome} from './ConfigureHome';
 
-export {SplashScreen, Login, Home, Add_Product, Product};
+export {SplashScreen, Login, Home, Add_Product, Product, ConfigureHome};
