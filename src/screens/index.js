f5b8a059ddb5ddfb1ea6e5e290a4904680f415ba
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-19 11:52:16 +0530

    Added Product page path to screens index

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,5 +2,6 @@ import {SplashScreen} from './SplashScreen';
 import {Login} from './Login';
 import {Home} from './Home';
 import {Add_Product} from './AddProduct';
+import {Product} from './Product';
 
-export {SplashScreen, Login, Home, Add_Product};
+export {SplashScreen, Login, Home, Add_Product, Product};
