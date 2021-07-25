ec6d8eb89375da7c26dbdbd333e88d10058e37ed
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-07-25 16:28:54 +0530

    fixed some typo

--- src/screens/QRCodeScanner.js
+++ src/screens/QRCodeScanner.js
@@ -1,53 +1,51 @@
 import React from 'react';
-import {View, Linking} from 'react-native';
+import {View, Linking, StyleSheet, TouchableOpacity, Text} from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
 
 export const QRCode = () => {
   const onSuccess = e => {
     Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err)
+      console.error('An error occured', err),
     );
   };
 
-    return (
-      <QRCodeScanner
-        onRead={() => onSuccess()}
-        flashMode={RNCamera.Constants.FlashMode.torch}
-        topContent={
-          <Text style={styles.centerText}>
-            Go to
-            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-            your computer and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-      />
-    );
-  }
-
+  return (
+    <QRCodeScanner
+      onRead={() => onSuccess()}
+      flashMode={RNCamera.Constants.FlashMode.torch}
+      topContent={
+        <Text style={styles.centerText}>
+          Go to
+          <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+          your computer and scan the QR code.
+        </Text>
+      }
+      bottomContent={
+        <TouchableOpacity style={styles.buttonTouchable}>
+          <Text style={styles.buttonText}>OK. Got it!</Text>
+        </TouchableOpacity>
+      }
+    />
+  );
+};
 
 const styles = StyleSheet.create({
   centerText: {
     flex: 1,
     fontSize: 18,
     padding: 32,
-    color: '#777'
+    color: '#777',
   },
   textBold: {
     fontWeight: '500',
-    color: '#000'
+    color: '#000',
   },
   buttonText: {
     fontSize: 21,
-    color: 'rgb(0,122,255)'
+    color: 'rgb(0,122,255)',
   },
   buttonTouchable: {
-    padding: 16
-  }
+    padding: 16,
+  },
 });
-};
