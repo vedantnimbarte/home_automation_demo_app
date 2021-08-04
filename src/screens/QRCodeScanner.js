c11cee7f925297ad16d73e7545241cfab1c04dc9
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-08-04 13:28:36 +0530

    Removed some bugs

--- src/screens/QRCodeScanner.js
+++ src/screens/QRCodeScanner.js
@@ -1,29 +1,63 @@
 import React from 'react';
-import {View, Linking, StyleSheet, TouchableOpacity, Text} from 'react-native';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  ActivityIndicator,
+  ToastAndroid,
+} from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
+import {CONFIG} from '../constants/config';
+import {COLORS, FONTS, SIZES} from '../constants/theme';
 
-export const QRCode = () => {
-  const onSuccess = e => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err),
-    );
+export const QRCode = ({navigation}) => {
+  const [status, setStatus] = React.useState('Scanning');
+  const [loading, setLoading] = React.useState(true);
+
+  const onSuccess = async e => {
+    let data = e.data.split('|');
+    let date = new Date().toISOString().slice(0, 10);
+    let formattedDate = date.split('-');
+    let created_at = `${formattedDate[0]}/${formattedDate[1]}/${formattedDate[2]}`;
+
+    const response = await fetch(
+      `http://${CONFIG.IP}:${CONFIG.PORT}/config/addDeviceToInventory?device=${data[0]}&type=${data[1]}&no_of_relays=${data[2]}&amp=${data[3]}&serial=${data[4]}&date=${created_at}`,
+    ).catch(error => {
+      ToastAndroid.show('Network request failed.', ToastAndroid.LONG);
+      navigation.goBack();
+    });
+    const result = await response.json();
+    if (result.success === 1) {
+      setLoading(false);
+      setStatus(result.message);
+      ToastAndroid.show(`${result.message}`, ToastAndroid.LONG);
+      setTimeout(() => {
+        navigation.goBack();
+      }, 2000);
+    } else {
+      ToastAndroid.show('Unable to add device', ToastAndroid.LONG);
+    }
   };
 
   return (
     <QRCodeScanner
-      onRead={() => onSuccess()}
-      flashMode={RNCamera.Constants.FlashMode.torch}
+      onRead={onSuccess}
+      flashMode={RNCamera.Constants.FlashMode.off}
       topContent={
         <Text style={styles.centerText}>
-          Go to
-          <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-          your computer and scan the QR code.
+          Place QR Code inside the window to register the device
         </Text>
       }
       bottomContent={
         <TouchableOpacity style={styles.buttonTouchable}>
-          <Text style={styles.buttonText}>OK. Got it!</Text>
+          <Text style={styles.buttonText}>
+            {loading ? (
+              <ActivityIndicator color={COLORS.Primary} size="large" />
+            ) : (
+              `Status: ${status}`
+            )}
+          </Text>
         </TouchableOpacity>
       }
     />
